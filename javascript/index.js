6c40fa697725e3b2a6b9d7f4e8ac6c240d4d77cc
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-03 21:23:42 +0100

    learning javascript and how to write to consoles

---
+++ javascript/index.js
@@ -0,0 +1,60 @@
+// This is my first javascript console
+// console.log("Hello World");
+
+// learning variables
+
+// let name = "Lawson";
+
+// console.log(name);
+
+// let firstName;
+
+// learning constant
+
+// const interestRate = 0.3;
+// interestRate = 1;
+
+// console.log(interestRate);
+
+// learning datatype
+
+// let age = 30.1; // Number literal
+// let name = "Mosh"; // string literal
+// let isApproved = true; // Boolean Literal
+// let firstName = undefined;
+// let selectedColor = null;
+
+// typeof name;
+
+// name = 1;
+// console.log(typeof firstName);
+
+
+// let person = {
+//     name: "Mosh",
+//     age: 30
+// };
+
+// // Dot notation
+// person.name = 'John';
+
+// // bracket notation
+// person['name'] = 'Mary';
+
+// console.log(person.name);
+
+// Arrays
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1
+// console.log(selectedColors.length);
+
+// learning functions
+function greet(name, lastName) {
+    // body of function
+    console.log("Hello " + name + " " + lastName);
+}
+
+let name = 'Lawson';
+greet(name);
+greet("Kelvin");
