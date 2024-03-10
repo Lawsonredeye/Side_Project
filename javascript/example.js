765d2104b4fd0296f1399bc0626f647cf1351375
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-10 18:21:11 +0100

    learnt about loops and did an exercise

---
+++ javascript/example.js
@@ -0,0 +1,61 @@
+/* learning how to create an object with javascript */
+// create a const var
+// add property and property value to it about a user enclosed in curly brackets
+// give the object a characteristic like ring() for a phone
+// const telephone = {
+// 	"brand":"iphone",
+// 	"date-created": "2023-09-01",
+// 	ring_out: () => console.log("Ringing"), 
+// }
+//
+// brandName = telephone.brand;
+// brandName = telephone["date-created"];
+// console.log(brandName);
+// telephone.ring_out();
+// for (var i = 0; i < 3; i++)
+// {
+// 	console.log(i);
+// // }
+// var myArray = ["A", "B", "C"];
+// for (var i = 0; i < myArray.length; i++)
+// {
+//     console.log("The member of myArray in index " + i + " is " + myArray[i]);
+// }
+//
+// Testing how to use while loops and conditionals
+// var i = 99;
+// 
+// while (true)
+// {
+//     console.log(i + " bottles of beer on the wall");
+//     i -= 1;
+//     if (i == 0)
+// 	break;
+// }
+
+/* Testing the continue statement */
+// for (var i = 0; i < 100; i++)
+// {
+//     if (i % 2 == 0)
+// 	continue;
+//     console.log(i + ' is an odd number')
+// }
+//
+// Exercise
+// write a for loop that iterates on the myArray variable and ptints out all of its members.
+//
+// Solution:
+// 1. find out the lenght of all of its members
+// create a variable to store the value of the length/ amount of members
+// 2. create a counter var and initialize it to 0 to keep track of the array members
+// 3. using a for loop add the counter as the initialization and the create a check to see if the counter is less than the array lenght. increment the counter by 1 or counter++.
+// 4. print out the members.
+
+// var myArray = ["What is the meaning of life?", "The meaning of life is", 42];
+//
+// array_length = myArray.length;
+// var counter = 0;
+// for (counter; counter < array_length; counter++)
+// {
+//     console.log(myArray[counter]);
+// }
